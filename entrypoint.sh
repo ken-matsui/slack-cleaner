5a8a2ffb9ebd0f431b8d1ed1b740b3f24e1a0d44
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2019-12-09 09:42:44 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,19 @@
-#!/bin/sh -l
+#!/bin/bash
 
-echo "hello $1"
+ARGS=()
+ARGS+=(--token $INPUT_TOKEN)
+[ -n "$INPUT_RATE"           ] && ARGS+=(--rate "$INPUT_RATE")
+[ "$INPUT_MESSAGE" == 'true' ] && ARGS+=(--message)
+[ "$INPUT_FILE"    == 'true' ] && ARGS+=(--file)
+[ -n "$INPUT_CHANNEL"        ] && ARGS+=(--channel "$INPUT_CHANNEL")
+[ -n "$INPUT_DIRECT"         ] && ARGS+=(--direct "$INPUT_DIRECT")
+[ -n "$INPUT_GROUP"          ] && ARGS+=(--group "$INPUT_GROUP")
+[ -n "$INPUT_MPDIRECT"       ] && ARGS+=(--mpdirect "$INPUT_MPDIRECT")
+[ -n "$INPUT_USER"           ] && ARGS+=(--user "$INPUT_USER")
+[ "$INPUT_BOT" == 'true'     ] && ARGS+=(--bot)
+[ -n "$INPUT_AFTER"          ] && ARGS+=(--after "$INPUT_AFTER")
+[ -n "$INPUT_BEFORE"         ] && ARGS+=(--before "$INPUT_BEFORE")
+[ -n "$INPUT_TYPES"          ] && ARGS+=(--types "$INPUT_TYPES")
+[ "$INPUT_PERFORM" == 'true' ] && ARGS+=(--perform)
+echo "slack-cleaner ${ARGS[@]}"
+slack-cleaner "${ARGS[@]}"
